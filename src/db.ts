4f93467e478fa187c40c74233b829f62859ced91
MikhailZharkov <zharkov@abcreg.ru>
2018-08-02 14:10:54 +0300

    config

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize-typescript';
-import { DB_CONFIG } from '../config/database.config'
+import { DB_CONFIG } from './config/database.config';
 
 const db = new Sequelize({
     name: DB_CONFIG.name,
